f8415350325ab3b3700cb1328f3e3e718c24481a
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-01-26 16:44:59 +0000

    final del curso

--- MiProyecto/Program.cs
+++ MiProyecto/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var corona = new Beer("corona");
+
+System.Console.WriteLine(ToUpper(corona).Name);
+System.Console.WriteLine(corona.Name);
+
+Beer ToUpper(Beer beer)
+{
+    var beer2 = new Beer(beer.Name.ToUpper());
+    return beer2;
+}
+
+class Beer
+{
+    public string Name { get; set; }
+
+    public Beer(string name)
+    {
+        this.Name = name;
+    }
+}
